6170578c27c199a79f98acef8031c9ac50340df5
Jonas Westerlund <jonas.westerlund@helsinki.fi>
2022-03-12 21:37:05 +0200

    Revert removal of timeseries library and add legacy-peer-deps to solve peer dependency resolving for react v17

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import UserChart from '../components/UserChart';
+import UserChart from '../components/UserChart';
 import DrinkChart from '../components/DrinkChart';
 import FeatFormDialog from '../components/FeatFormDialog';
 import Paper from '@mui/material/Paper';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import _ from 'lodash';
-// import ProjectionChart from '../components/ProjectionChart';
+import ProjectionChart from '../components/ProjectionChart';
 
 // En ny chart för timeseries behövs, den som används här stöder int React v17
 class Statistics extends React.Component {
@@ -34,9 +34,9 @@ class Statistics extends React.Component {
                         {/* <Tab label="Projektion"/> */}
                     </Tabs>
                 </Paper>
-                {/* {this.state.tab === 0 && <UserChart store={this.props.store}/>} */}
+                {this.state.tab === 0 && <UserChart store={this.props.store}/>}
                 {this.state.tab === 0 && <DrinkChart store={this.props.store}/>}
-                {/* {this.state.tab === 2 && <ProjectionChart store={this.props.store}/>} */}
+                {this.state.tab === 2 && <ProjectionChart store={this.props.store}/>}
             </div>
         );
     }
