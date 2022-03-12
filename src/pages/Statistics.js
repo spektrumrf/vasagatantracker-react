1c8cebf481adb21069cf9e23b9a786dd89a7f5db
Jonas Westerlund <jonas.westerlund@helsinki.fi>
2022-03-12 21:33:39 +0200

    Removed incompatible timeseries chartin library until better one is found and implemented

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import UserChart from '../components/UserChart';
+// import UserChart from '../components/UserChart';
 import DrinkChart from '../components/DrinkChart';
 import FeatFormDialog from '../components/FeatFormDialog';
 import Paper from '@mui/material/Paper';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import _ from 'lodash';
-import ProjectionChart from '../components/ProjectionChart';
+// import ProjectionChart from '../components/ProjectionChart';
 
+// En ny chart för timeseries behövs, den som används här stöder int React v17
 class Statistics extends React.Component {
     constructor(props) {
         super(props);
@@ -28,14 +29,14 @@ class Statistics extends React.Component {
                 <FeatFormDialog store={this.props.store}/>}
                 <Paper square>
                     <Tabs value={this.state.tab} indicatorColor="primary" textColor="primary" onChange={this.handleChange}>
-                        <Tab label="Poäng"/>
+                        {/* <Tab label="Poäng"/> */}
                         <Tab label="Drycker"/>
-                        <Tab label="Projektion"/>
+                        {/* <Tab label="Projektion"/> */}
                     </Tabs>
                 </Paper>
-                {this.state.tab === 0 && <UserChart store={this.props.store}/>}
-                {this.state.tab === 1 && <DrinkChart store={this.props.store}/>}
-                {this.state.tab === 2 && <ProjectionChart store={this.props.store}/>}
+                {/* {this.state.tab === 0 && <UserChart store={this.props.store}/>} */}
+                {this.state.tab === 0 && <DrinkChart store={this.props.store}/>}
+                {/* {this.state.tab === 2 && <ProjectionChart store={this.props.store}/>} */}
             </div>
         );
     }
